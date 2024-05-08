786521264645fe7db347f356b8d8156720187f43
David Gomez <lordraum@gmail.com>
2024-05-08 16:50:17 -0500

    correct config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://github.com/lordraum/',
+  site: 'https://lordraum.github.io/',
   base: 'actos-de-memoria',
 })
